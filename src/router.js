057e4721043b57236a3758755e7d43dc835ae8e7
Luan Barbosa <luanqbarbosa@gmail.com>
2021-05-31 11:25:34 -0300

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
+import NotFound from './pages/NotFound.vue';
+import store from './store/index';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: CoachesList },
+        { 
+            path: '/coaches/:id',
+            component: CoachDetails,
+            props: true,
+            children: [
+                { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+            ] 
+        },
+        { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
+        { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+        { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+        { path: '/:notFound(.*)', component: NotFound }
+    ]
+});
+
+router.beforeEach(function(to, from, next) {
+    if(to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if(to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches');
+    } else {
+        next();
+    }
+});
+
+export default router;
